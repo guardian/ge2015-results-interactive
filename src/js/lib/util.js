eb44d2d02ece2d908733c4a1c519067d9afe2325
Matt Osborn <matthew.osborn@theguardian.com>
2015-05-07 03:35:40 +0100

    addclass fn

--- src/js/lib/util.js
+++ src/js/lib/util.js
@@ -3,6 +3,11 @@ export function removeClass(el, className) {
     else el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 }
 
+export function addClass(el, className) {
+    if (el.classList) el.classList.add(className);
+    else el.className += ' ' + className;
+}
+
 export function throttle(callback, limit) {
     var wait = false;
     return function () {
