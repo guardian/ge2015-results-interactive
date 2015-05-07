7437d9bdb98f4588b42a627a5f3a16cd5aa5f5a2
Will Franklin <will.franklin@guardian.co.uk>
2015-05-07 15:06:53 +0100

    Enable data download

--- src/js/snap.js
+++ src/js/snap.js
@@ -13,7 +13,7 @@ class ElectionSnap {
         this.analysis = new ByConstituency(el.querySelector('#analysis'));
         //this.analysis = new Coalitions(el.querySelector('#analysis'));
 
-        //window.setInterval(() => this.fetchDataAndRender(), 5000);
+        window.setInterval(() => this.fetchDataAndRender(), 5000);
         this.fetchDataAndRender();
     }
 
